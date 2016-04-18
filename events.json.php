152216268f39acc05a381c105b47f95f9156d8f2
unknown <wsx741405@gmail.com>
2016-04-18 18:01:45 +0800

    midProject_12_1. 增加活動事件 2. 修改calendar下拉框顏色

--- events.json.php
+++ events.json.php
@@ -3,11 +3,57 @@
 	"result": [
 		{
 			"id": "001",
-			"title": "",
+			"title": "資工系讀書週",
+			"category": "資工系學會",
 			"url": "",
 			"class": "event-inverse",
-			"start": "1460307200000",
-			"end":   "1460307286400"
+			"start": "1460368800000",
+			"end":   "1460383200000"
+		},
+		{
+			"id": "001",
+			"title": "資工系讀書週",
+			"category": "資工系學會",
+			"url": "",
+			"class": "event-inverse",
+			"start": "1460455200000",
+			"end":   "1460469600000"
+		},
+		{
+			"id": "001",
+			"title": "資工系讀書週",
+			"category": "資工系學會",
+			"url": "",
+			"class": "event-inverse",
+			"start": "1460541600000",
+			"end":   "1460556000000"
+		},
+		{
+			"id": "001",
+			"title": "資工系讀書週",
+			"category": "資工系學會",
+			"url": "",
+			"class": "event-inverse",
+			"start": "1460628000000",
+			"end":   "1460642400000"
+		},
+		{
+			"id": "001",
+			"title": "資工系讀書週",
+			"category": "資工系學會",
+			"url": "",
+			"class": "event-inverse",
+			"start": "1460714400000",
+			"end":   "1460728800000"
+		},
+		{
+			"title": "期中考週",
+			"category": "北科大活動",
+			"id": "002",
+			"url": "",
+			"class": "event-inverse",
+			"start": "1460908800000",
+			"end":   "1461254400000"
 		}
 	]
 }
